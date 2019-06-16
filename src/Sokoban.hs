dca06c154e4cb6d3590335aceca4bc7bdd800f11
raphael meyer <raphael.akira.meyer@gmail.com>
2019-06-16 12:52:13 +0200

    move around

--- src/Sokoban.hs
+++ src/Sokoban.hs
@@ -12,6 +12,9 @@ data Graphics = Graphics {
   getFigure :: Sf.RectangleShape
 }
 
+data Event = MoveDown | MoveUp | MoveLeft | MoveRight
+type Events = [Event]
+
 run :: IO ()
 run = do
   let game = GameState (5,5)
@@ -27,32 +30,47 @@ run = do
 
 loop :: Graphics -> GameState -> IO ()
 loop graphics game = do
-  let window = getWindow graphics
-  result <- processEvents window game
+  result <- processEvents $ getWindow graphics
   case result of
     Nothing -> return ()
-    Just game' -> do
-      let figure = getFigure graphics
-      Sf.clearRenderWindow window $ Sf.Color 240 240 240 255
-      Sf.setPosition figure $ figurePosition game'
-      Sf.drawRectangle window (getFigure graphics) Nothing
-      Sf.display window
+    Just events -> do
+      let game' = updateGame game events
+      render graphics game'
       loop graphics game'
 
-processEvents :: Sf.RenderWindow -> GameState -> IO (Maybe GameState)
-processEvents window game = do
+updateGame :: GameState -> Events -> GameState
+updateGame game [] = game
+updateGame game (MoveDown:es) = updateGame (move game (0,1)) es
+updateGame game (MoveUp:es) = updateGame (move game (0,-1)) es
+updateGame game (MoveLeft:es) = updateGame (move game (-1,0)) es
+updateGame game (MoveRight:es) = updateGame (move game (1,0)) es
+
+render :: Graphics -> GameState -> IO ()
+render graphics game = do
+  let figure = getFigure graphics
+  let window = getWindow graphics
+  Sf.clearRenderWindow window $ Sf.Color 240 240 240 255
+  Sf.setPosition figure $ figurePosition game
+  Sf.drawRectangle window (getFigure graphics) Nothing
+  Sf.display window
+
+processEvents :: Sf.RenderWindow -> IO (Maybe Events)
+processEvents window = do
   event <- Sf.pollEvent window
   case event of
     Just Sf.SFEvtClosed -> return Nothing
     Just Sf.SFEvtKeyPressed{Sf.code = Sf.KeyEscape} -> return Nothing
-    Just Sf.SFEvtKeyPressed{Sf.code = Sf.KeyDown} -> return . Just $ moveDown game
-    Nothing -> return . Just $ game
-    _ -> processEvents window game
+    Just Sf.SFEvtKeyPressed{Sf.code = Sf.KeyDown} -> fmap (MoveDown:) <$> processEvents window
+    Just Sf.SFEvtKeyPressed{Sf.code = Sf.KeyUp} -> fmap (MoveUp:) <$> processEvents window
+    Just Sf.SFEvtKeyPressed{Sf.code = Sf.KeyLeft} -> fmap (MoveLeft:) <$> processEvents window
+    Just Sf.SFEvtKeyPressed{Sf.code = Sf.KeyRight} -> fmap (MoveRight:) <$> processEvents window
+    Nothing -> return (Just [])
+    _ -> processEvents window
 
 figurePosition :: GameState -> Sf.Vec2f
 figurePosition game =
   let (x,y) = getPosition game
   in Sf.Vec2f (fromIntegral $ 64 * x) (fromIntegral $ 64 * y)
 
-moveDown :: GameState -> GameState
-moveDown game = GameState . ( \(x,y) -> (x,y+1) ) $ getPosition game
+move :: GameState -> (Int,Int) -> GameState
+move game@GameState{getPosition = (x,y)} (dx,dy) = game { getPosition = (x+dx,y+dy) }
