66dfc92e5b42258db88157940e03d21dcbbaf0b9
Raphael Meyer <raphael.akira.meyer@gmail.com>
2019-06-17 14:27:37 +0200

    add tile sheet
    Signed-off-by: raphael meyer <raphael.akira.meyer@gmail.com>

--- src/Graphics.hs
+++ src/Graphics.hs
@@ -9,32 +9,39 @@ module Graphics (
 import qualified SFML.Graphics as Sf
 import qualified SFML.Window as Sf
 
+import Paths_Sokoban
+
 import Game
 
 data Graphics = Graphics {
   getWindow :: Sf.RenderWindow,
-  getFigure :: Sf.RectangleShape
+  getFigure :: Sf.RectangleShape,
+  getTilesheet :: Sf.Texture
 }
 
 initialize :: IO Graphics
 initialize = do
   let context = Just $ Sf.ContextSettings 24 8 0 1 2 [Sf.ContextDefault]
   window <- Sf.createRenderWindow (Sf.VideoMode 640 480 32) "Sokoban" [Sf.SFDefaultStyle] context
+  tilesheetPath <- getDataFileName "sokoban_tilesheet.png"
+  tilesheet <- Sf.err $ Sf.textureFromFile tilesheetPath Nothing
   figure <- Sf.err Sf.createRectangleShape
-  Sf.setFillColor figure Sf.blue
   Sf.setSize figure $ Sf.Vec2f 64 64
-  return $ Graphics window figure
+  Sf.setTexture figure tilesheet True
+  Sf.setTextureRect figure $ Sf.IntRect 0 256 64 64
+  return $ Graphics window figure tilesheet
 
 cleanup :: Graphics -> IO ()
 cleanup g = do
   Sf.destroy $ getFigure g
+  Sf.destroy $ getTilesheet g
   Sf.destroy $ getWindow g
 
 render :: Graphics -> GameState -> IO ()
 render graphics game = do
   let figure = getFigure graphics
   let window = getWindow graphics
-  Sf.clearRenderWindow window $ Sf.white
+  Sf.clearRenderWindow window Sf.white
   Sf.setPosition figure $ figurePosition game
   Sf.drawRectangle window (getFigure graphics) Nothing
   Sf.display window
